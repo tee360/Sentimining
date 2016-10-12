4bf32212964246b9d9a3e667c99b609f21de49c0
Thomas Martin <thomas.martin4@wayne.edu>
2016-10-12 12:01:27 -0400

    added SentimentAnalyzer.java

---
+++ src/main/java/co/grandcircus/finalproject/model/SentimentAnalyzer.java
@@ -0,0 +1,41 @@
+package co.grandcircus.finalproject.model;
+
+/**
+ * Responsibility: Provide additional movie info fromNetflix API
+ */
+public class SentimentAnalyzer {
+
+	/**
+	 * Returns string of 'positive' or 'negative', depending on user input.
+	 */
+	private String type;
+	/**
+	 * Average score analysis of entire string.
+	 */
+	private double score;
+	/**
+	 * Strategic selection of words to analyze from the string.
+	 */
+	private String word;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+		
+
+}
