bf1e81619d530df7c2f2bb051c70c7d377d30e05
Thomas Martin <thomas.martin4@wayne.edu>
2016-10-13 11:24:33 -0400

    Continued work on Sentiment API connect

--- src/main/java/co/grandcircus/finalproject/rest/SentimentAnalyzerService.java
+++ src/main/java/co/grandcircus/finalproject/rest/SentimentAnalyzerService.java
@@ -13,6 +13,7 @@ import com.google.gson.JsonParser;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import co.grandcircus.finalproject.model.SentimentAnalyzer;
 @Service
@@ -21,16 +22,22 @@ public class SentimentAnalyzerService {
 	private final static String STRING_RATING = "positive";
 	private final static String HAPPY_STRING = "I love this place";
 
-	public SentimentAnalyzer getSentimentAnalysisForHappyString() {
+	public HttpResponse<JsonNode> getSentimentAnalysisForHappyString() {
 		return getAnalysisOfSentiment(HAPPY_STRING);
 	}
 	
-	public SentimentAnalyzer getAnalysisOfSentiment(String tweet) {
-		HttpResponse<JsonNode> jsonResponse = Unirest.get("https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=great+value+in+its+price+range!")
-				.header("X-Mashape-Key", "vqKLeNUFrDmshfUd60DopAwITbrJp19qzOyjsnAURqfBjeFQ9w")
-				.header("Accept", "application/json")
-				.routeParam("text", tweet)
-				.asJson();
+	public HttpResponse<JsonNode> getAnalysisOfSentiment(String tweet) {
+		HttpResponse<JsonNode> jsonResponse = null;
+		try {
+			jsonResponse = Unirest.get("https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=great+value+in+its+price+range!")
+					.header("X-Mashape-Key", "vqKLeNUFrDmshfUd60DopAwITbrJp19qzOyjsnAURqfBjeFQ9w")
+					.header("Accept", "application/json")
+					.routeParam("text", tweet)
+					.asJson();
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 //		try (BufferedReader reader = HttpResponse.doGet) { // try with resources will auto close the reader
 //			if (reader == null) {
@@ -46,7 +53,7 @@ public class SentimentAnalyzerService {
 //		stringAnalyzer.setType(jObject.get("type").getAsString());
 //		stringAnalyzer.setScore(jObject.get("score").getAsDouble());
 		
-		return stringAnalyzer;
+		return jsonResponse;
 //		String url = "https://twinword-sentiment-analysis.p.mashape.com/analyze/";
 //		// Use HTTP GET with the above URL
 //		try (BufferedReader reader = HttpHelper.doGet(url)) { // try with resources will auto close the reader
