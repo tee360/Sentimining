4bb682271fd4f86803807804928f5b6fdb62aaf4
narmasaru <narmasarav@gmail.com>
2016-10-14 13:44:13 -0400

    Updated DAO

--- src/main/java/co/grandcircus/finalproject/rest/SentimentAnalyzerService.java
+++ src/main/java/co/grandcircus/finalproject/rest/SentimentAnalyzerService.java
@@ -11,7 +11,7 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-import co.grandcircus.finalproject.dao.Twitterdaojdbc;
+
 import co.grandcircus.finalproject.model.SentimentAnalyzer;
 import co.grandcircus.finalproject.model.Twitter;
 
@@ -106,42 +106,7 @@ public class SentimentAnalyzerService {
 		
 		return stringAnalyzer;
 		
-//		// parse the HTTP response body to JSON
-		//JsonElement root = new JsonObject();
-		 
-		
-//		String url = "https://twinword-sentiment-analysis.p.mashape.com/analyze/";
-//		// Use HTTP GET with the above URL
-//		try (BufferedReader reader = HttpHelper.doGet(url)) { // try with resources will auto close the reader
-//			if (reader == null) {
-//				throw new RuntimeException("Not found: " + url);
-//			}
-//			
-//			// parse the HTTP response body to JSON
-//			JsonElement root = new JsonParser().parse(reader);
-//			// 
-//			JsonObject jObject = root.getAsJsonObject();
-//			//JsonArray keywords = root.getAsJsonObject().get("keywords").getAsJsonArray();
-//			
-//			//ArrayList<SentimentAnalyzer> keywordList = new ArrayList<SentimentAnalyzer>();
-//			SentimentAnalyzer stringAnalyzer = new SentimentAnalyzer();
-//			
-////			for(int i = 0; i < keywords.size(); i++) {
-////				stringAnalyzer.setWord(keywords.get(i).getAsJsonObject().get("word").getAsString());
-////			}
-//
-//			// 
-//			stringAnalyzer.setType(jObject.get("type").getAsString());
-//			stringAnalyzer.setScore(jObject.get("score").getAsDouble());
-//			// 
-//		//	stringAnalyzer.setWord(jObject.get("word").getAsString());
-//			// 
-//			
-//
-//			return stringAnalyzer;
-//		} catch (IOException ex) {
-//			throw new RuntimeException("Error reading from URL: " + url, ex);
-//		}
+
 	}
 	
 }
